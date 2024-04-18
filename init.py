b91cca9efcf3b479ca84ac72d22b00e680ba4fb3
amadouissa9 <amadouissaamadou9@gmail.com>
2024-04-18 00:47:12 -0400

    revision

--- init.py
+++ init.py
@@ -2,6 +2,6 @@ from utilisateurs.utilisateur_dao import UtilisateurDao
 #from utilisateur import Utilisateur
 
 utilisateur_dao = UtilisateurDao()
-message = utilisateur_dao.add_utilisateur('Bijou', 'tresor', '35', 'bij@test.com', '12345', 'Admin')
-
+message = utilisateur_dao.add_utilisateur('armel', 'joel', '40', 'bij@tes.com', '12345', 'Admin')
+#message = utilisateur_dao.modifier_utilisateur('bij@test.com')
 print(message)
--- utilisateurs/utilisateur_dao.py
+++ utilisateurs/utilisateur_dao.py
@@ -10,6 +10,31 @@ class UtilisateurDao:
             params =(nom, prenom, age, email,password, role)
             cls.cursor.execute(sql, params)
             cls.connecte.commit()
-            print(f'Mr/Mme {nom, prenom} a été(e) ajouté(e) avec succès!')
+            print(f'Mr/Mme {nom} {prenom} a été(e) ajouté(e) avec succès!')
         except Exception as e:
             print("Erreur d'insertion !",e)
+    
+    def client_existe(self, email):
+        # Vérifie si un utilisateur avec l'email donné existe dans la base de données
+        sql = "SELECT COUNT(*) FROM utilisateur WHERE Email_util = %s"
+        self.cursor.execute(sql, (email,))
+        count = self.cursor.fetchone()[0]
+        return count > 0
+    
+    @classmethod
+    def modifier_utilisateur(self, email, id_utilisateur, nouveau_nom, nouveau_prenom, nouveau_sexe):
+        try:
+            # Vérification de l'existence de l'utilisateur
+            utilisateur_existe = self.client_existe(email)
+            if not utilisateur_existe:
+                print("L'utilisateur avec cet email n'existe pas.")
+                return
+
+            # L'utilisateur existe, procéder à la modification
+            sql = "UPDATE utilisateur SET ID_utilisateur = %s, Nom_util = %s, Prenom_util = %s, Sexe_util = %s WHERE Email_util = %s"
+            values = (id_utilisateur, nouveau_nom, nouveau_prenom, nouveau_sexe, email)
+            self.cursor.execute(sql, values)
+            self.connection.commit()
+            print("Utilisateur modifié avec succès !")
+        except Exception as err:
+            print(f"Erreur lors de la modification de l'utilisateur : {err}")
